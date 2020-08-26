9875d13070a2d49e643af19916fa5fa929eb760b
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-08-26 09:18:15 -0500

    Initial commit

---
+++ src/Window.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Koala
+ */
+public class Window {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
