f7b62ed08b743f7ae23fe2d262c553b3355e76ce
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-09-03 20:52:58 -0500

    Update KeyBoard.java

--- src/input/KeyBoard.java
+++ src/input/KeyBoard.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  *
  * @author Koala
  */
-public class KeyBoard {
+
+//Esta clase se encarga de todo lo que ingrese por teclado.
+//Implementamos keyListener para que escuche por teclado
+public class KeyBoard  implements KeyListener{
+    public KeyBoard(){
     
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
 }
