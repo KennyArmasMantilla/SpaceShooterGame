c8c17ab974a71bf3e0d2e0d8f9246715f6362068
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-09-11 22:57:05 -0500

    Constants
    Variables Constantes

---
+++ src/gameObjects/Constants.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameObjects;
+
+/**
+ *
+ * @author Koala
+ */
+public class Constants {
+    //DIMENSIONES DEL FRAME
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    
+    //PROPIEDAS DE JUGADOR
+    public static final int FIRERATE=300;
+    public static final double DELTAANGLE=0.1;
+    public static final double ACC=0.08;//Representa cuanto queremos que acelere la nave
+    public static final double PLAYER_MAX_VEL=7.0;
+    
+    //PROPIEDASDES DEL LASER
+    
+    public static final double LASER_VEL=15.0;
+    
+    
+    
+}
