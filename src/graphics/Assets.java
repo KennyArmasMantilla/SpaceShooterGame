1dd041cb46b437f9aea7311d8f3fc58eb16ec0ff
KennyArmasMantilla <60169312+KennyArmasMantilla@users.noreply.github.com>
2020-09-24 15:02:52 -0500

    Update Assets.java

--- src/graphics/Assets.java
+++ src/graphics/Assets.java
@@ -32,10 +32,27 @@ public class Assets {
     //----------------Explosiones--------------------------
     public static BufferedImage[] exp = new BufferedImage[9];
     
+    //-----------------ENEMYS------------------------------
+    //BLACK
+    public static BufferedImage[] enemiesBlack = new BufferedImage[5];
+    //BLUE
+    public static BufferedImage[] enemiesBlues = new BufferedImage[5];
+    //GREEN
+    public static BufferedImage[] enemiesGreen = new BufferedImage[5];
+    //RED
+    public static BufferedImage[] enemiesRed = new BufferedImage[5];
+    
+    //------------UFO-------------------------------------
+    public static BufferedImage ufo;
+    
+    
     
     public static void init(){
         //Nave
         player = Loader.ImageLoader("/ships/player.png");
+        
+        //ufo
+        ufo = Loader.ImageLoader("/ships/ufoBlue.png");
         
         //Propulsion
         speed=Loader.ImageLoader("/effects/fire08.png");
@@ -84,6 +101,28 @@ public class Assets {
         for(int i = 0; i<exp.length; i++){
             exp[i]=Loader.ImageLoader("/explosion/exp"+(i+1)+".png");
         }
+        
+        //-------------enemies-------------------
+       //BLACK
+        for(int i = 0; i<enemiesBlack.length; i++){
+            enemiesBlack[i]=Loader.ImageLoader("/enemies/enemyBlack"+(i+1)+".png");
+        }
+       //BLUES
+       for(int i = 0; i<enemiesBlues.length; i++){
+           enemiesBlues[i]=Loader.ImageLoader("/enemies/enemyBlue"+(i+1)+".png");
+       }
+       //GREEN
+       for(int i = 0; i<enemiesGreen.length; i++){
+           enemiesGreen[i]=Loader.ImageLoader("/enemies/enemyGreen"+(i+1)+".png");
+       }
+       //RED
+       for(int i = 0; i<enemiesRed.length; i++){
+           enemiesRed[i]=Loader.ImageLoader("/enemies/enemyRed"+(i+1)+".png");
+       }
+        
+       //UFO
+       
+        
         
     }
 }
